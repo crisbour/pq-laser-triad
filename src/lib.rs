320ec38c3b16fb10de059d166d7336cc71f39c21
Cristian Bourceanu <v.c.bourceanu@sms.ed.ac.uk>
2025-02-04 15:06:32 +0000

    Declare test mod instead in order to import

--- src/lib.rs
+++ src/lib.rs
@@ -7,17 +7,19 @@ include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
 mod errors;
 pub use errors;
 
-#[test]
-fn test_get_version() {
-    // import all of our stuff
-    use super::super::*;
+#[cfg(test)]
+mod test{
     use super::*;
-    println!("OUT_DIR={}", env!("OUT_DIR"));
-    unsafe {
-        let mut cLibVersion: [i8; 256] = [0; 256];
-        // TODO: Change return to Error type
-        let iRetVal = SEPIA2_LIB_GetVersion(cLibVersion.as_mut_ptr());
-        assert_eq!(iRetVal, 0);
-        println!("Version: {}", std::ffi::CStr::from_ptr(cLibVersion.as_ptr()).to_str().unwrap());
+    fn test_get_version() {
+        // import all of our stuff
+        use super::*;
+        println!("OUT_DIR={}", env!("OUT_DIR"));
+        unsafe {
+            let mut cLibVersion: [i8; 256] = [0; 256];
+            // TODO: Change return to Error type
+            let iRetVal = SEPIA2_LIB_GetVersion(cLibVersion.as_mut_ptr());
+            assert_eq!(iRetVal, 0);
+            println!("Version: {}", std::ffi::CStr::from_ptr(cLibVersion.as_ptr()).to_str().unwrap());
+        }
     }
 }
